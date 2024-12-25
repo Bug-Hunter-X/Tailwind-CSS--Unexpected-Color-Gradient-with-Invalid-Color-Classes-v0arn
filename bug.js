0fb2170ab2faefc2ce478400fd470ad061177552
BugHunter-X <askzenai@gmail.com>
2024-12-25 08:18:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,5 @@
+```javascript
+// Example using Tailwind's arbitrary value feature
+<div class="bg-gradient-to-r from-red-500 to-yellow-500 h-64"></div>
+```
+This may cause an unexpected color gradient if the values specified in `from` and `to` are not valid Tailwind CSS color classes.
